5996f1b09de08702ecdf9fe9a09bbc03b68d37e1
Vesa Vainio <vesa.vainio@nordpoolgroup.com>
2017-03-25 23:50:24 +0200

    Refactoring to OOP style done, compiles, not tested

--- watering1/watering1/watering1.ino
+++ watering1/watering1/watering1.ino
@@ -18,11 +18,6 @@ dht DHT;
 #define DHT_MODE_OK 1
 #define DHT_MODE_ERROR -1
 
-#define DISP_MODE_CURRENT 1
-#define DISP_MODE_1H 2
-#define DISP_MODE_6H 3
-#define DISP_MODE_STATS 4
-
 #define MENU_MODE_SHOW_HISTORY 1
 #define MENU_MODE_EXIT 99
 
@@ -42,7 +37,7 @@ const int oneHourSeriesBytes = 24 * sizeof(float);
 
 byte dhtMode = DHT_MODE_OK;
 
-byte dispMode = DISP_MODE_STATS;
+byte dispMode = 0;
 unsigned long lcdUpdatedMillis = 0;
 
 int currentTemperature = 0;
@@ -60,155 +55,6 @@ int button1State = 0;
 int button2State = 0;
 bool buttonStateChanging = false;
 
-int mode = MODE_1_DISP_CYCLE;
-
-int menuMode = MENU_MODE_SHOW_HISTORY;
-
-
-void setup()
-{
-  Serial.begin(9600);
-
-  lcd.begin(16,2);
-
-  pinMode(MOISTURE_PIN1, OUTPUT);
-  pinMode(MOISTURE_PIN2, OUTPUT);
-
-  pinMode(BUTTON1_PIN, INPUT);
-  pinMode(BUTTON2_PIN, INPUT);
-  
-  for(int i = 0; i< 3; i++)
-  {
-    digitalWrite(MOISTURE_PIN1, HIGH);
-    digitalWrite(MOISTURE_PIN2, LOW);
-    lcd.noBacklight();
-    delay(250);
-
-    currentSoil = analogRead(0); // get the moisture reading
-    
-    digitalWrite(MOISTURE_PIN1, LOW);
-    digitalWrite(MOISTURE_PIN2, HIGH);
-    lcd.backlight();
-    delay(250);
-  }
-
-  digitalWrite(MOISTURE_PIN1, LOW);
-  digitalWrite(MOISTURE_PIN2, LOW);
-}
-
-void loop()
-{
-  updateDht();
-  updateMoisture();
-  updateButtonsWithDebounce();
-  
-  if (dhtMode == DHT_MODE_OK) 
-  {
-      doSampling();
-      updateLcd();
-  }
-
-  delay(5);
-}
-
-void updateButtonsWithDebounce()
-{
-  int button1NewState = digitalRead(BUTTON1_PIN);
-  int button2NewState = digitalRead(BUTTON2_PIN);
-
-  unsigned long currentMillis = millis();
-  if (buttonStateChanging == false && (button1NewState != button1State || button2NewState != button2State))
-  {
-    buttonUpdatedMillis == currentMillis;
-    buttonStateChanging = true;
-  }
-  else if (buttonStateChanging = true && currentMillis > buttonUpdatedMillis + 50)
-  {
-    if (button1State == LOW && button1NewState == HIGH)
-    {
-      button1Pressed();
-    }
-
-    if (button2State == LOW && button2NewState == HIGH)
-    {
-      button2Pressed();
-    }
-
-    button1State = button1NewState;
-    button2State = button2NewState;
-    buttonStateChanging = false;
-  }
-}
-
-void button1Pressed()
-{
-  if (mode == MODE_1_DISP_CYCLE)
-  {
-    mode = MODE_2_DISP_MENU;
-    menuMode = MENU_MODE_SHOW_HISTORY;
-    renderMenu();
-  }
-  else if (mode == MODE_2_DISP_MENU)
-  {
-    menuNext();
-    renderMenu();
-  }
-  else if (mode == MODE_3_DISP_HISTORY)
-  {
-    mode = MODE_1_DISP_CYCLE;
-    lcdUpdatedMillis = 0;
-    dispMode = DISP_MODE_STATS;
-  }
-}
-
-void button2Pressed()
-{
-  menuChoose();
-}
-
-void renderMenu()
-{
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("MENU");
-  lcd.setCursor(0,1);
-  if (menuMode == MENU_MODE_SHOW_HISTORY)
-  {
-    lcd.print("    SHOW HISTORY");
-  }
-  else if (menuMode == MENU_MODE_EXIT)
-  {
-    lcd.print("    EXIT");
-  }
-}
-
-void menuNext()
-{
-  if (menuMode == MENU_MODE_SHOW_HISTORY)
-  {
-    menuMode = MENU_MODE_EXIT;
-  }
-  else if (menuMode == MENU_MODE_EXIT)
-  {
-    menuMode = MENU_MODE_SHOW_HISTORY;
-  }
-}
-
-void menuChoose()
-{
-  if (menuMode == MENU_MODE_EXIT)
-  {
-    mode = MODE_1_DISP_CYCLE;
-    lcdUpdatedMillis = 0;
-    dispMode = DISP_MODE_STATS;
-  }
-  else if (menuMode == MENU_MODE_SHOW_HISTORY)
-  {
-    mode = MODE_3_DISP_HISTORY;
-    lcdUpdatedMillis = 0;
-    dispMode = 0;
-  }
-}
 
 void updateMoisture()
 {
@@ -235,110 +81,6 @@ void updateMoisture()
     digitalWrite(MOISTURE_PIN1, LOW);
     digitalWrite(MOISTURE_PIN2, LOW);
     moistureUpdatedMillis = currentMillis;
-  }
-}
-
-void updateLcd()
-{
-  if (mode == MODE_1_DISP_CYCLE)
-  {
-    updateLcdWithDisplayCycle();
-  }
-  else if (mode == MODE_3_DISP_HISTORY)
-  {
-    updateLcdWithHistory();
-  }
-}
-
-void updateLcdWithHistory()
-{
-  unsigned long currentMillis = millis();
-  if (lcdUpdatedMillis == 0 || currentMillis > lcdUpdatedMillis + 400)
-  {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    if (dispMode <= 11)
-    {
-      int minuteIndex = getMinuteIndex() - 1 - dispMode;
-      if (minuteIndex < 0)
-      {
-        minuteIndex += 12;
-      }
-
-      int minutes = (dispMode + 1) * 5;
-      if (minutes < 10)
-      {
-        lcd.print(" ");
-      }
-      
-      lcd.print(String(minutes) + "m " + String(getMinuteSample(0, minuteIndex), 1) + " " + String(getMinuteSample(1, minuteIndex), 1));
-      lcd.setCursor(0,1);
-      lcd.print("    " + String(getMinuteSample(2, minuteIndex), 1));
-    }
-    else {
-      int hourIndex = getHourIndex() - 1 - (dispMode - 12);
-      if (hourIndex < 0)
-      {
-        hourIndex += 24;  
-      }
-
-      int hours = (dispMode - 11);
-      if (hours < 10)
-      {
-        lcd.print(" ");
-      }
-
-      lcd.print(String(hours) + "h " + String(getHourSample(0, hourIndex), 1) + " " + String(getHourSample(1, hourIndex), 1));
-      lcd.setCursor(0,1);
-      lcd.print("    " + String(getHourSample(2, hourIndex), 1));
-    }
-    
-    dispMode += 1;
-    if (dispMode > 35)
-    {
-      dispMode = 0;
-    }
-    lcdUpdatedMillis = currentMillis;
-  }
-}
-
-void updateLcdWithDisplayCycle()
-{
-  unsigned long currentMillis = millis();
-  if (lcdUpdatedMillis == 0 || currentMillis > lcdUpdatedMillis + 2000)
-  {
-    lcd.clear();
-    lcd.setCursor(0,0);
-
-    if (dispMode == DISP_MODE_STATS)
-    {
-      dispMode = DISP_MODE_CURRENT;
-      lcd.print("tmp  " + String(currentTemperature) + " hmd " + String(currentHumidity));
-      lcd.setCursor(0,1);
-      lcd.print("soil " + String(currentSoil));
-    } 
-    else if (dispMode == DISP_MODE_CURRENT)
-    {
-      dispMode = DISP_MODE_1H;
-      lcd.print("1h " + String(getNHoursAvg(0, 1), 1) + " " + String(getNHoursAvg(1, 1), 1));
-      lcd.setCursor(0,1);
-      lcd.print("   " + String(getNHoursAvg(2, 1), 1) + "  ");
-    }
-    else if (dispMode == DISP_MODE_1H)
-    {
-      dispMode = DISP_MODE_6H;
-      lcd.print("6h " + String(getNHoursAvg(0, 6), 1) + " " + String(getNHoursAvg(1, 6), 1));
-      lcd.setCursor(0,1);
-      lcd.print("   " + String(getNHoursAvg(2, 6), 1));
-    }
-    else if (dispMode == DISP_MODE_6H)
-    {
-      dispMode = DISP_MODE_STATS;
-      lcd.print("run " + String(currentMillis/(1000*60ul*60ul)) + "h " + String((currentMillis % (1000*60ul*60ul)) / (1000*60ul)) + "min");
-      lcd.setCursor(0,1);
-      lcd.print(String(currentMillis));
-    }
-    lcdUpdatedMillis = currentMillis;
   }
 }
 
@@ -484,5 +226,292 @@ int getHourIndex() {
   int index;
   EEPROM.get(2, index);
   return index;
+}
+
+class DisplayHandler {
+  public:
+    virtual DisplayHandler* button1Pressed() { return this; }
+    virtual DisplayHandler* button2Pressed() { return this; }
+    virtual void activate() {};
+    virtual void updateLcd() {};
+};
+
+class HistoryRoller;
+class InfoRoller;
+
+class MainMenu : public DisplayHandler {
+  private:
+    char *menuItems[2] = { "SHOW HISTORY", "EXIT" };
+    int itemIndex = 0;
+
+    void printMenuOnLcd() {
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("MENU");
+      lcd.setCursor(0,1);
+      lcd.print(menuItems[itemIndex]);
+    };
+      
+  public:
+    DisplayHandler *_InfoRoller;
+    DisplayHandler *_HistoryRoller;    
+    
+    virtual DisplayHandler* button1Pressed() { 
+      itemIndex = itemIndex++ % 2;
+      printMenuOnLcd();
+      return this;
+    };
+    
+    virtual DisplayHandler* button2Pressed() { 
+      switch (itemIndex) {
+        case 0:
+          return this;
+        case 1:
+          return _HistoryRoller;
+      }
+    };
+
+    virtual void activate() {
+      printMenuOnLcd();
+    }
+    
+    virtual void updateLcd() { };
+
+    void Init(DisplayHandler* infoRoller, DisplayHandler* historyRoller) {
+      _InfoRoller = infoRoller;
+      _HistoryRoller = historyRoller;
+    }
+};
+
+class InfoRoller : public DisplayHandler {
+  private:
+    MainMenu *_MainMenu;
+
+    enum Mode {
+      Current,
+      Hours1,
+      Hours6,
+      Stats
+    };
+
+    Mode mode;
+    unsigned long lcdUpdatedMillis;
+  
+  public:
+    InfoRoller(MainMenu* mainMenu) {
+      _MainMenu = mainMenu;
+      mode = Current;
+      lcdUpdatedMillis = 0;
+    }
+    
+    virtual DisplayHandler* button1Pressed() {
+      return _MainMenu;
+    }
+    
+    virtual DisplayHandler* button2Pressed() { }
+
+    virtual void activate() {
+      lcdUpdatedMillis = 0;
+    };
+
+    virtual void updateLcd() { 
+      unsigned long currentMillis = millis();
+      if (lcdUpdatedMillis == 0 || currentMillis > lcdUpdatedMillis + 2000)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+    
+        if (mode == Stats)
+        {
+          mode = Current;
+          lcd.print("tmp  " + String(currentTemperature) + " hmd " + String(currentHumidity));
+          lcd.setCursor(0,1);
+          lcd.print("soil " + String(currentSoil));
+        } 
+        else if (mode == Current)
+        {
+          mode = Hours1;
+          lcd.print("1h " + String(getNHoursAvg(0, 1), 1) + " " + String(getNHoursAvg(1, 1), 1));
+          lcd.setCursor(0,1);
+          lcd.print("   " + String(getNHoursAvg(2, 1), 1) + "  ");
+        }
+        else if (mode == Hours1)
+        {
+          mode = Hours6;
+          lcd.print("6h " + String(getNHoursAvg(0, 6), 1) + " " + String(getNHoursAvg(1, 6), 1));
+          lcd.setCursor(0,1);
+          lcd.print("   " + String(getNHoursAvg(2, 6), 1));
+        }
+        else if (mode == Hours6)
+        {
+          mode = Stats;
+          lcd.print("run " + String(currentMillis/(1000*60ul*60ul)) + "h " + String((currentMillis % (1000*60ul*60ul)) / (1000*60ul)) + "min");
+          lcd.setCursor(0,1);
+          lcd.print(String(currentMillis));
+        }
+        
+        lcdUpdatedMillis = currentMillis;
+      }  
+    };
+};
+
+class HistoryRoller : public DisplayHandler {
+  private:
+    MainMenu *_MainMenuLocal;
+    unsigned long lcdUpdatedMillis;
+  public:
+    HistoryRoller(MainMenu* mainMenu) {
+      _MainMenuLocal = mainMenu;
+    }
+
+    virtual DisplayHandler* button1Pressed() { return _MainMenuLocal; }
+    virtual DisplayHandler* button2Pressed() { return this; }
+
+    virtual void activate() {
+      lcdUpdatedMillis = 0;
+    };
+
+    virtual void updateLcd() {
+      unsigned long currentMillis = millis();
+      if (lcdUpdatedMillis == 0 || currentMillis > lcdUpdatedMillis + 400)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        if (dispMode <= 11)
+        {
+          int minuteIndex = getMinuteIndex() - 1 - dispMode;
+          if (minuteIndex < 0)
+          {
+            minuteIndex += 12;
+          }
+    
+          int minutes = (dispMode + 1) * 5;
+          if (minutes < 10)
+          {
+            lcd.print(" ");
+          }
+          
+          lcd.print(String(minutes) + "m " + String(getMinuteSample(0, minuteIndex), 1) + " " + String(getMinuteSample(1, minuteIndex), 1));
+          lcd.setCursor(0,1);
+          lcd.print("    " + String(getMinuteSample(2, minuteIndex), 1));
+        }
+        else {
+          int hourIndex = getHourIndex() - 1 - (dispMode - 12);
+          if (hourIndex < 0)
+          {
+            hourIndex += 24;  
+          }
+    
+          int hours = (dispMode - 11);
+          if (hours < 10)
+          {
+            lcd.print(" ");
+          }
+    
+          lcd.print(String(hours) + "h " + String(getHourSample(0, hourIndex), 1) + " " + String(getHourSample(1, hourIndex), 1));
+          lcd.setCursor(0,1);
+          lcd.print("    " + String(getHourSample(2, hourIndex), 1));
+        }
+        
+        dispMode += 1;
+        if (dispMode > 35)
+        {
+          dispMode = 0;
+        }
+        lcdUpdatedMillis = currentMillis;
+      }
+    };
+};
+
+MainMenu *_MainMenu = new MainMenu();
+InfoRoller *_InfoRoller = new InfoRoller(_MainMenu);
+HistoryRoller *_HistoryRoller = new HistoryRoller(_MainMenu);
+
+DisplayHandler *currentHandler;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  lcd.begin(16,2);
+
+  pinMode(MOISTURE_PIN1, OUTPUT);
+  pinMode(MOISTURE_PIN2, OUTPUT);
+
+  pinMode(BUTTON1_PIN, INPUT);
+  pinMode(BUTTON2_PIN, INPUT);
+  
+  for(int i = 0; i< 3; i++)
+  {
+    digitalWrite(MOISTURE_PIN1, HIGH);
+    digitalWrite(MOISTURE_PIN2, LOW);
+    lcd.noBacklight();
+    delay(250);
+
+    currentSoil = analogRead(0); // get the moisture reading
+    
+    digitalWrite(MOISTURE_PIN1, LOW);
+    digitalWrite(MOISTURE_PIN2, HIGH);
+    lcd.backlight();
+    delay(250);
+  }
+
+  digitalWrite(MOISTURE_PIN1, LOW);
+  digitalWrite(MOISTURE_PIN2, LOW);
+
+  _MainMenu->Init(_InfoRoller, _HistoryRoller);
+  currentHandler = _InfoRoller;
+}
+
+void updateButtonsWithDebounce()
+{
+  int button1NewState = digitalRead(BUTTON1_PIN);
+  int button2NewState = digitalRead(BUTTON2_PIN);
+
+  unsigned long currentMillis = millis();
+  if (buttonStateChanging == false && (button1NewState != button1State || button2NewState != button2State))
+  {
+    buttonUpdatedMillis == currentMillis;
+    buttonStateChanging = true;
+  }
+  else if (buttonStateChanging = true && currentMillis > buttonUpdatedMillis + 50)
+  {
+    DisplayHandler* oldCurrentHandler = currentHandler;
+    DisplayHandler* newCurrentHandler;
+
+    if (button1State == LOW && button1NewState == HIGH)
+    {
+      newCurrentHandler = currentHandler->button1Pressed();
+    }
+
+    if (button2State == LOW && button2NewState == HIGH)
+    {
+      newCurrentHandler = currentHandler->button2Pressed();
+    }
+
+    if (oldCurrentHandler != newCurrentHandler) {
+      currentHandler = newCurrentHandler;
+      currentHandler->activate();
+    }
+
+    button1State = button1NewState;
+    button2State = button2NewState;
+    buttonStateChanging = false;
+  }
+}
+
+void loop()
+{
+  updateDht();
+  updateMoisture();
+  updateButtonsWithDebounce();
+  
+  if (dhtMode == DHT_MODE_OK) 
+  {
+      doSampling();
+      currentHandler->updateLcd();
+  }
+
+  delay(5);
 }
 
