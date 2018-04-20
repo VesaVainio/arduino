9d9296bfc5bed2c73fff6187d7fc3dab1e5b7b36
Vesa Vainio <vesatvainio@gmail.com>
2018-04-20 23:32:06 +0300

    First logic for moving the hatch based on temperature

--- HatchControl/HatchControl.ino
+++ HatchControl/HatchControl.ino
@@ -14,8 +14,8 @@
 #include "EepromInterface.h"
 
 #define MOTOR_ENABLE_PIN 2
-#define MOTOR_DRIVE_1 3
-#define MOTOR_DRIVE_2 4
+#define MOTOR_UP_PIN 3
+#define MOTOR_DOWN_PIN 4
 
 #define BUTTON1_PIN 8
 #define BUTTON2_PIN 9
@@ -27,7 +27,7 @@ dht DHT;
 MeasuringContext* measuringContext = new MeasuringContext();
 
 InfoDisplay* _InfoDisplay = new InfoDisplay(&lcd, measuringContext);
-TestMenu* _TestMenu = new TestMenu(&lcd, _InfoDisplay, MOTOR_ENABLE_PIN, MOTOR_DRIVE_1, MOTOR_DRIVE_2);
+TestMenu* _TestMenu = new TestMenu(&lcd, _InfoDisplay, MOTOR_ENABLE_PIN, MOTOR_UP_PIN, MOTOR_DOWN_PIN);
 
 DisplayHandler* currentHandler = _InfoDisplay;
 
@@ -80,11 +80,45 @@ void updateButtonsWithDebounce()
 	}
 }
 
+unsigned long hatchChangedMillis = 0;
+
+void updateHatch() {
+	unsigned long currentMillis = millis();
+	int hatchPosition = getHatchPosition();
+	int currentTemp = measuringContext->getCurrentTemperature();
+	if (currentMillis > hatchChangedMillis + 3 * 60 * 1000) {
+		if (currentTemp > 27 && hatchPosition < 5) {
+			lcd.clear();
+			lcd.print("Raising to " + String(hatchPosition + 1) + "/5");
+			digitalWrite(MOTOR_ENABLE_PIN, HIGH);
+			digitalWrite(MOTOR_UP_PIN, HIGH);
+			delay(1500);
+			digitalWrite(MOTOR_ENABLE_PIN, LOW);
+			digitalWrite(MOTOR_UP_PIN, LOW);
+			putHatchPosition(hatchPosition + 1);
+			hatchChangedMillis = currentMillis;
+			currentHandler->activate();
+		}
+		else if (currentTemp < 24 && hatchPosition > 0) {
+			lcd.clear();
+			lcd.print("Lowering to " + String(hatchPosition - 1) + "/5");
+			digitalWrite(MOTOR_ENABLE_PIN, HIGH);
+			digitalWrite(MOTOR_DOWN_PIN, HIGH);
+			delay(1500);
+			digitalWrite(MOTOR_ENABLE_PIN, LOW);
+			digitalWrite(MOTOR_DOWN_PIN, LOW);
+			putHatchPosition(hatchPosition - 1);
+			hatchChangedMillis = currentMillis;
+			currentHandler->activate();
+		}
+	}
+}
+
 void setup()
 {
 	pinMode(MOTOR_ENABLE_PIN, OUTPUT);
-	pinMode(MOTOR_DRIVE_1, OUTPUT);
-	pinMode(MOTOR_DRIVE_2, OUTPUT);
+	pinMode(MOTOR_UP_PIN, OUTPUT);
+	pinMode(MOTOR_DOWN_PIN, OUTPUT);
 
 	pinMode(BUTTON1_PIN, INPUT);
 	pinMode(BUTTON2_PIN, INPUT);
@@ -115,6 +149,8 @@ void loop()
 	bool dhtError = measuringContext->updateDht(DHT, DHT11_PIN);
 
 	updateButtonsWithDebounce();
+
+	updateHatch();
 
 	currentHandler->updateLcd();
 
