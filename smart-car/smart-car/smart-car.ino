6818fafcb281787ef12accc32cfe91b9efec1ce9
Vesa Vainio <vesatvainio@gmail.com>
2019-02-03 14:07:51 +0200

    smart-car testing motors

--- smart-car/smart-car/smart-car.ino
+++ smart-car/smart-car/smart-car.ino
@@ -4,6 +4,16 @@
 const int trigPin = 6;
 const int echoPin = 7;
 
+const int motor1enable = 10;
+const int motor1forward = 9;
+const int motor1backward  = 8;
+
+
+const int motor2enable = 5;
+const int motor2forward = 4;
+const int motor2backward = 3;
+
+
 unsigned long lastScreenUpdate;
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
@@ -14,13 +24,25 @@ void setup()
   
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  
+  pinMode(motor1enable, OUTPUT);
+  pinMode(motor1forward, OUTPUT);
+  pinMode(motor1backward, OUTPUT);
+  pinMode(motor2enable, OUTPUT);
+  pinMode(motor2forward, OUTPUT);
+  pinMode(motor2backward, OUTPUT); 
+
+  analogWrite(motor1enable, 150);
+
+  analogWrite(motor2enable, 150);
+
 
   lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
 
   for (int i = 0; i < 3; i++)
   {
     lcd.backlight();
-    delay(250);
+    delay(250); 
     lcd.noBacklight();
     delay(250);
   }
@@ -31,6 +53,18 @@ void setup()
 
   lcd.setCursor(0,0);
   lcd.print("Distance: ");
+  
+  delay(5000);
+  digitalWrite(motor1forward, HIGH);
+  digitalWrite(motor2forward, LOW);  
+ 
+  delay(5000);
+  digitalWrite(motor1forward, LOW);
+  digitalWrite(motor2forward, HIGH);  
+
+  delay(5000);
+  digitalWrite(motor1forward, LOW);
+  digitalWrite(motor2forward, LOW);  
 }
 
 void loop()
